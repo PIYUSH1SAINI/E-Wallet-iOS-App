82690f9d8c6ae6e99f7be05493a989daa43a41cb
PIYUSH1SAINI <piyushsaini20001@gmail.com>
2024-07-24 09:30:34 +0530

    Initial Commit

---
+++ Aashes/AashesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AashesApp.swift
+//  Aashes
+//
+//  Created by Piyush saini on 24/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AashesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
